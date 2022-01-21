b3354a80fc36203402d39b3455166138b00b7880
Meke Finau <lonifinau.09@gmail.com>
2022-01-21 12:19:50 -0700

    week-1-assesment

---
+++ sldc.js
@@ -0,0 +1,37 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Analysis of Requirements
+// Design
+// Implementation
+// Maintain
+// Planning
+// Testing & Integration
+
+Planning
+Analysis of requirements
+Design
+Implementation
+Testing & Integration
+Maintain
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+let analysis = 'this is the second step, where you see if your plan reach the requirements of the website you are about to design.'
+let design = 'this is the third step, is where you make decisions on the layout of your website.'
+let implementation = 'this is the fourth step, which is when you start coding.'
+let testing = 'this is the fifth step, when you need to test your codes.'
+let maintain = 'this is the sixth step, keep it up to date and fix bugs.'
+
+
+
+
+
